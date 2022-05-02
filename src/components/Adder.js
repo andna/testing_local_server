81f88feef0416a67974f0c0df2fa07b3d94e256a
Andres Bastidas <andresbastidas@quiena.com>
2022-05-02 13:14:52 -0300

    4 name validation (may used yup)

--- src/components/Adder.js
+++ src/components/Adder.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 export default function Adder({handleAdd}) {
+
+    const [ formTouched, setFormTouched ] = useState(false);
+    const [ formValid, setFormValid ] = useState(false);
+    const [ name, setName ] = useState("");
+    const [ nameError, setNameError ] = useState("");
+
+    const onNameChange = (e) => {
+        setName(e.target.value)
+    }
+
+    useEffect(()=> {
+        let isValid = true;
+        if(name){
+            if(!formTouched){
+                setFormTouched(true)
+            }
+            if(name.length >= 25){
+                isValid = false;
+                setNameError("Can't be longer than 25 chars.")
+            } else {
+                setNameError("");
+            }
+        } else {
+            setNameError("Name required")
+        }
+    }, [name])
 
     return  <div className="container">
         <form>
             <div className="form-group">
                 <label>Name</label>
-                <input className="form-control"/>
+                <input
+                    value={name}
+                    onChange={(e)=> onNameChange(e)}
+                    className="form-control"/>
+                {formTouched && nameError && nameError !== "" &&
+                <p className="msg-error">{nameError}</p>}
             </div>
             <div className="form-group">
                 <label>Payment Status</label>
